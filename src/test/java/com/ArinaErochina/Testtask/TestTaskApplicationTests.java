c4f454059f3851b22cb402637f4ef62975c1f776
Lu <lokka.rei@yandex.ru>
2023-04-05 12:34:14 +0700

    start comment

---
+++ src/test/java/com/ArinaErochina/Testtask/TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArinaErochina.Testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
